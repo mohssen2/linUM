e4da66990816af31cdd8b56350fe3faf835b1a76
nutgut <tommy.hecken@gmail.com>
2017-10-27 18:24:40 +0200

    Lade till kommentarer

---
+++ libpower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "power.h"
+//library 2 by Tommy
+//Calculates totally power process in a circuit (in 2 ways)
+float calc_power_r(float volt, float resistance)
+{
+	float p;
+	p=volt*resistance;
+	return p;
+}
+
+float calc_power_i(float volt, float current)
+{
+	float p;
+	p=(volt*volt)/current;
+	return p;
+}
+
---
+++ power.h
@@ -0,0 +1,9 @@
+#ifndef __power_h__
+#define __power_h__
+
+//This header defines the functions in libpower.c by Tommy
+float calc_power_r(float volt, float resistance);
+float calc_power_i(float volt, float current);
+
+#endif
+
